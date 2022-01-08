41b1024b00812bcdacd1074188f065dde65b7ec3
tomtkg <39430259+tomtkg@users.noreply.github.com>
2022-01-08 12:25:32 +0900

    Update evaluator.py

--- eval/evaluator.py
+++ eval/evaluator.py
@@ -1,6 +1,7 @@
 import math
 import subprocess
 
+D = 34        # 支給対象を決める遺伝子長．設計変数の次元数．
 N_PROC = 6    # 子プロセスの展開数．
 FID = "[1]"   # 目的関数のID．"[1]" or "[2]" or "[1,2]"
 CITY = "naha" # 実行する都市名．"naha" or "hakodate"
@@ -9,40 +10,41 @@ CITY = "naha" # 実行する都市名．"naha" or "hakodate"
 def gene2pay(gene):
     q = ''
     
-    family_type_val = [1, 2, 3, 4, 50, 60, 70, 80]
-    family_type = [family_type_val[j] for i,j in zip(range(0, 8), range(8)) if gene[i] == 1]
+    family_type_val = [1, 2, 50, 60, 70, 80]
+    family_type = [family_type_val[j] for i,j in zip(range(0, 6), range(6)) if gene[i] == 1]
     family_type = ",".join(map(str, family_type))
-    q = q + 'family_type_id == [0,' + family_type + ']'
+    q = q + 'family_type_id == [0,3,4,' + family_type + ']'
 
     role_household_type_val = [10, 11, 20, 21, 30, 31]
-    role_household_type = [role_household_type_val[j] for i,j in zip(range(8, 14), range(6)) if gene[i] == 1]
+    role_household_type = [role_household_type_val[j] for i,j in zip(range(6, 12), range(6)) if gene[i] == 1]
     role_household_type = ",".join(map(str, role_household_type))
     q = q + ' and role_household_type_id == [0,1,' + role_household_type + ']'
 
     industry_type_val = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
-    industry_type = [industry_type_val[j] for i,j in zip(range(14, 34), range(20)) if gene[i] == 1]
+    industry_type = [industry_type_val[j] for i,j in zip(range(12, 32), range(20)) if gene[i] == 1]
     industry_type = ",".join(map(str, industry_type))
     q = q + ' and industry_type_id == [-1,' + industry_type + ']'
 
-    employment_type_val = [10, 30]
-    employment_type = [employment_type_val[j] for i,j in zip(range(34, 36), range(2)) if gene[i] == 1]
+    employment_type_val = [10]
+    employment_type = [employment_type_val[j] for i,j in zip(range(32, 33), range(1)) if gene[i] == 1]
     employment_type = ",".join(map(str, employment_type))
-    q = q + ' and employment_type_id == [-1,20,' + employment_type + ']'        
+    q = q + ' and employment_type_id == [-1,20,30,' + employment_type + ']'        
 
-    company_size_val = [10 ,100, 1000]
-    company_size = [company_size_val[j] for i,j in zip(range(36, 39), range(3)) if gene[i] == 1]
+    company_size_val = [1000]
+    company_size = [company_size_val[j] for i,j in zip(range(33, 34), range(1)) if gene[i] == 1]
     company_size = ",".join(map(str, company_size))
-    q = q + ' and company_size_id == [-1,5,' + company_size + ']'
+    q = q + ' and company_size_id == [-1,5,10,100,' + company_size + ']'
 
     return q
 
 # 子プロセスが完了することを待って，最適な値mを返す
 def get_optm(p):
     a, _ = p.communicate(timeout=1_000)
+    a_split = eval(a)
     if CITY == "naha":
-        x = 5613300000 / (5613300000 - min(eval(a)[-1]))
+        x = 5613300000 / (5613300000 - min(a_split[-1]))
     else:
-        x = 4752780000 / (4752780000 - min(eval(a)[-1]))
+        x = 4752780000 / (4752780000 - min(a_split[-1]))
     return math.floor(x * 100) / 100
 
 # 子プロセスが完了することを待って，適応度を返す
@@ -54,9 +56,8 @@ def ret_fitness(p):
     else:
         return float(a_split[0]), float(a_split[2])
 
-# 個体の評価を行う
-def evaluation(pop):
-    f1_list, f2_list, m_list = [], [], []
+# popをN_PROC個を単位として，バッチに分ける．
+def get_batch_list(pop):
     n_ind = len(pop)
     batch_list, ind_list = [], []
     for i in range(n_ind):
@@ -64,13 +65,18 @@ def evaluation(pop):
         if (i + 1) % N_PROC == 0 or i == n_ind - 1:
             batch_list.append(ind_list)
             ind_list = []
+    return batch_list
+
+# 個体の評価を行う
+def evaluation(pop):
+    f1_list, f2_list, m_list = [], [], []
+    batch_list = get_batch_list(pop)
             
     for ind_list in batch_list:
-        job_list, procs = [], []
+        job_list = []
+
         for i in ind_list:
-            q = gene2pay(pop[i])
-            cmd = ["python", "eval/syn_pop.py", str(q), "1", FID, CITY, "[42]"]
-            job_list.append(cmd)
+            job_list.append(["python", "eval/syn_pop.py", gene2pay(pop[i]), "1", FID, CITY, "[42]"])
         procs = [subprocess.Popen(job, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) for job in job_list]
 
         for i in range(len(ind_list)):
@@ -80,8 +86,8 @@ def evaluation(pop):
         procs = [subprocess.Popen(job, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) for job in job_list]
 
         for i in range(len(ind_list)):
-            avg_1, avg_2 = ret_fitness(procs[i])
-            f1_list = f1_list + [avg_1]
-            f2_list = f2_list + [avg_2]
+            f1, f2 = ret_fitness(procs[i])
+            f1_list = f1_list + [f1]
+            f2_list = f2_list + [f2]
 
     return f1_list, f2_list, m_list
