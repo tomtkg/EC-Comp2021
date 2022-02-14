a3fd7fba68283b910b570c3be929e970b342e1c3
tomtkg <39430259+tomtkg@users.noreply.github.com>
2022-02-14 23:32:50 +0900

    Create exp.bat

---
+++ exp.bat
@@ -0,0 +1,56 @@
+python conv_sop0.py naha [42] 0 1> result/naha42conv0_0_solution.csv 2> result/naha42conv0_0_summary.csv
+python conv_sop0.py naha [42] 1 1> result/naha42conv0_1_solution.csv 2> result/naha42conv0_1_summary.csv
+python conv_sop0.py naha [42] 2 1> result/naha42conv0_2_solution.csv 2> result/naha42conv0_2_summary.csv
+python conv_sop0.py naha [42] 3 1> result/naha42conv0_3_solution.csv 2> result/naha42conv0_3_summary.csv
+python conv_sop0.py naha [42] 4 1> result/naha42conv0_4_solution.csv 2> result/naha42conv0_4_summary.csv
+python conv_sop0.py naha [256] 0 1> result/naha256conv0_0_solution.csv 2> result/naha256conv0_0_summary.csv
+python conv_sop0.py naha [256] 1 1> result/naha256conv0_1_solution.csv 2> result/naha256conv0_1_summary.csv
+python conv_sop0.py naha [256] 2 1> result/naha256conv0_2_solution.csv 2> result/naha256conv0_2_summary.csv
+python conv_sop0.py naha [256] 3 1> result/naha256conv0_3_solution.csv 2> result/naha256conv0_3_summary.csv
+python conv_sop0.py naha [256] 4 1> result/naha256conv0_4_solution.csv 2> result/naha256conv0_4_summary.csv
+python conv_sop0.py hakodate [42] 0 1> result/hako42conv0_0_solution.csv 2> result/hako42conv0_0_summary.csv
+python conv_sop0.py hakodate [42] 1 1> result/hako42conv0_1_solution.csv 2> result/hako42conv0_1_summary.csv
+python conv_sop0.py hakodate [42] 2 1> result/hako42conv0_2_solution.csv 2> result/hako42conv0_2_summary.csv
+python conv_sop0.py hakodate [42] 3 1> result/hako42conv0_3_solution.csv 2> result/hako42conv0_3_summary.csv
+python conv_sop0.py hakodate [42] 4 1> result/hako42conv0_4_solution.csv 2> result/hako42conv0_4_summary.csv
+python conv_sop0.py hakodate [256] 0 1> result/hako256conv0_0_solution.csv 2> result/hako256conv0_0_summary.csv
+python conv_sop0.py hakodate [256] 1 1> result/hako256conv0_1_solution.csv 2> result/hako256conv0_1_summary.csv
+python conv_sop0.py hakodate [256] 2 1> result/hako256conv0_2_solution.csv 2> result/hako256conv0_2_summary.csv
+python conv_sop0.py hakodate [256] 3 1> result/hako256conv0_3_solution.csv 2> result/hako256conv0_3_summary.csv
+python conv_sop0.py hakodate [256] 4 1> result/hako256conv0_4_solution.csv 2> result/hako256conv0_4_summary.csv
+python conv_sop1.py naha [42] 0 1> result/naha42conv1_0_solution.csv 2> result/naha42conv1_0_summary.csv
+python conv_sop1.py naha [42] 1 1> result/naha42conv1_1_solution.csv 2> result/naha42conv1_1_summary.csv
+python conv_sop1.py naha [42] 2 1> result/naha42conv1_2_solution.csv 2> result/naha42conv1_2_summary.csv
+python conv_sop1.py naha [42] 3 1> result/naha42conv1_3_solution.csv 2> result/naha42conv1_3_summary.csv
+python conv_sop1.py naha [42] 4 1> result/naha42conv1_4_solution.csv 2> result/naha42conv1_4_summary.csv
+python conv_sop1.py naha [256] 0 1> result/naha256conv1_0_solution.csv 2> result/naha256conv1_0_summary.csv
+python conv_sop1.py naha [256] 1 1> result/naha256conv1_1_solution.csv 2> result/naha256conv1_1_summary.csv
+python conv_sop1.py naha [256] 2 1> result/naha256conv1_2_solution.csv 2> result/naha256conv1_2_summary.csv
+python conv_sop1.py naha [256] 3 1> result/naha256conv1_3_solution.csv 2> result/naha256conv1_3_summary.csv
+python conv_sop1.py naha [256] 4 1> result/naha256conv1_4_solution.csv 2> result/naha256conv1_4_summary.csv
+python conv_sop1.py hakodate [42] 0 1> result/hako42conv1_0_solution.csv 2> result/hako42conv1_0_summary.csv
+python conv_sop1.py hakodate [42] 1 1> result/hako42conv1_1_solution.csv 2> result/hako42conv1_1_summary.csv
+python conv_sop1.py hakodate [42] 2 1> result/hako42conv1_2_solution.csv 2> result/hako42conv1_2_summary.csv
+python conv_sop1.py hakodate [42] 3 1> result/hako42conv1_3_solution.csv 2> result/hako42conv1_3_summary.csv
+python conv_sop1.py hakodate [42] 4 1> result/hako42conv1_4_solution.csv 2> result/hako42conv1_4_summary.csv
+python conv_sop1.py hakodate [256] 0 1> result/hako256conv1_0_solution.csv 2> result/hako256conv1_0_summary.csv
+python conv_sop1.py hakodate [256] 1 1> result/hako256conv1_1_solution.csv 2> result/hako256conv1_1_summary.csv
+python conv_sop1.py hakodate [256] 2 1> result/hako256conv1_2_solution.csv 2> result/hako256conv1_2_summary.csv
+python conv_sop1.py hakodate [256] 3 1> result/hako256conv1_3_solution.csv 2> result/hako256conv1_3_summary.csv
+python conv_sop1.py hakodate [256] 4 1> result/hako256conv1_4_solution.csv 2> result/hako256conv1_4_summary.csv
+python propose.py naha [42] A 0 1> result/naha42proposeA0solution.csv 2> result/naha42proposeA0summary.csv
+python propose.py naha [42] A 1 1> result/naha42proposeA1solution.csv 2> result/naha42proposeA1summary.csv
+python propose.py naha [42] B 0 1> result/naha42proposeB0solution.csv 2> result/naha42proposeB0summary.csv
+python propose.py naha [42] B 1 1> result/naha42proposeB1solution.csv 2> result/naha42proposeB1summary.csv
+python propose.py naha [256] A 0 1> result/naha256proposeA0solution.csv 2> result/naha256proposeA0summary.csv
+python propose.py naha [256] A 1 1> result/naha256proposeA1solution.csv 2> result/naha256proposeA1summary.csv
+python propose.py naha [256] B 0 1> result/naha256proposeB0solution.csv 2> result/naha256proposeB0summary.csv
+python propose.py naha [256] B 1 1> result/naha256proposeB1solution.csv 2> result/naha256proposeB1summary.csv
+python propose.py hakodate [42] A 0 1> result/hako42proposeA0solution.csv 2> result/hako42proposeA0summary.csv
+python propose.py hakodate [42] A 1 1> result/hako42proposeA1solution.csv 2> result/hako42proposeA1summary.csv
+python propose.py hakodate [42] B 0 1> result/hako42proposeB0solution.csv 2> result/hako42proposeB0summary.csv
+python propose.py hakodate [42] B 1 1> result/hako42proposeB1solution.csv 2> result/hako42proposeB1summary.csv
+python propose.py hakodate [256] A 0 1> result/hako256proposeA0solution.csv 2> result/hako256proposeA0summary.csv
+python propose.py hakodate [256] A 1 1> result/hako256proposeA1solution.csv 2> result/hako256proposeA1summary.csv
+python propose.py hakodate [256] B 0 1> result/hako256proposeB0solution.csv 2> result/hako256proposeB0summary.csv
+python propose.py hakodate [256] B 1 1> result/hako256proposeB1solution.csv 2> result/hako256proposeB1summary.csv
